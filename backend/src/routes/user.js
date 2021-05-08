8f2b7872c88a3d266362a0e2c1e4f0643cff379d
Aniket762 <aniketindian8@gmail.com>
2021-05-09 03:29:26 +0530

    signup

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { signup } = require('../controller/user');
+
 
 router.post('/signin', (req, res) =>
 {
     
 });
 
-router.post('/signup', (req, res) =>
-{
-    
-});
+router.post('/signup', signup );
 
 module.exports = router;
